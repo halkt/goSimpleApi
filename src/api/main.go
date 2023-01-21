aadab51c00d4021b177c789645c6c4a810369696
“halmuda” <halmuda6231@gmail.com>
2023-01-21 22:49:12 +0900

    不要なパッケージ削除

--- src/api/main.go
+++ src/api/main.go
@@ -2,29 +2,68 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	DBMigrate(DBConnect())
+	db := DBMigrate(DBConnect())
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
+		// users := Users{}
+		// user := db.Find(&users)
 		c.JSON(200, gin.H{
 			"message": "成功",
+			"test": "hoge",
+		})
+	})
+	r.POST("/create", func(c *gin.Context) {
+		create(db, "D11", 1000)
+		c.JSON(200, gin.H{
+			"message": "登録成功",
+		})
+	})
+	r.GET("/404", func(c *gin.Context) {
+		c.JSON(404, gin.H{
+			"message": "404やで",
 		})
 	})
 	r.Run(":8080")
 }
 
+type Product struct {
+  gorm.Model
+  Code  string
+  Price uint
+}
+
 type User struct {
 	gorm.Model
 	NickName string `json:"nickName"`
+	Description string `json:"Description"`
 }
+
+type Blog struct {
+	gorm.Model
+	Title string `json:"Title"`
+}
+
+// Array of User
+type Users []User
 
 func DBMigrate(db *gorm.DB) *gorm.DB {
 	db.AutoMigrate(&User{})
+	db.AutoMigrate(&Blog{})
+	db.AutoMigrate(&Product{})
+	return db
+}
+
+func create(db *gorm.DB, code string, price uint) *gorm.DB {
+	db.Create(&Product{
+		Code: code,
+		Price: price,
+	})
 	return db
 }
 
